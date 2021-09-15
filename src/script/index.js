9970849f8f9f4d9f8fdbf34f3d73268b2e3cfa03
leojscalabrin <74567440+leojscalabrin@users.noreply.github.com>
2021-09-15 16:17:03 -0300

    menu json

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,55 @@
+const renderMenuDesktop = (menu) => {
+  let structureMenu = ''
+  const $containerMenu = document.querySelector(".header-menu")
+
+  menu.map((department) => {
+    if(department.children === undefined){
+      structureMenu += `
+        <div class="header-menu-wrapper">
+          <a href=${department.url}> ${department.name} </a>
+        </div>
+      `;
+    } else {
+      structureMenu += `
+        <div class="header-menu--wrapper">
+          <a href=${department.url}> ${department.name} </a>
+            <div class="header-menu--items">
+              ${department.children.map((category) => {
+                if(category.children === undefined) {
+                  return `<a href=${category.url}>${category.name}</a>`
+                } else {
+                  return `
+                  <div class="header-menu--wrapper-items">
+                    <a href=${category.url}>${category.name}</a>
+                    <div class="header-menu--sub-item">
+                      ${category.children.map((subcategory) => {
+                        return `<a href=${category.url}>${category.name}</a>`
+                      })}
+                    </div>
+                  </div>
+                  `
+                }
+              })}
+            </div>
+        </div>
+      `;
+    }
+  });
+
+  $containerMenu.innerHTML = structureMenu
+};
+
+const requestMenuDesktop = () => {
+  const menu = [];
+
+  fetch("./src/script/menu/menu.json")
+    .then((response) => response.json())
+    .then((json) => {
+      renderMenuDesktop(json.menu)
+    });
+};
+
+requestMenuDesktop();
 
 const bannerSwipper = () => {
     new Swiper(".container", {
